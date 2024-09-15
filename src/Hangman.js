2adf9344cdc6aea141733b4da79de2f5e85974e1
urmila1907 <urmilasangwan744@gmail.com>
2024-09-16 03:44:14 +0530

    small changes in logic

--- src/Hangman.js
+++ src/Hangman.js
@@ -14,14 +14,15 @@ function Hangman(){
   const [image, setImage] = useState(img0);
   const [guess,setGuess] = useState(new Set());
   const [wrong,setWrong] = useState(0);
-  const answer = randomWord();
+  const [answer] = useState(randomWord);
+  const maxWrong = 7;
 
   function guessed() {
     return answer.split("").map(ltr => (guess.has(ltr) ? ltr : "_")).join(" ");
   }
   function generateLetters() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
-      <button key={ltr} value={ltr} onClick={handleGuess} disabled={guess.has(ltr)}>
+      <button key={ltr} value={ltr} onClick={handleGuess} disabled={guess.has(ltr)|| gameOver() || gameWon()}>
         {ltr}
       </button>
     ));
@@ -32,19 +33,38 @@ function Hangman(){
     // Update the guessed letters set
     setGuess(st => new Set(st).add(ltr));
 
-    // Update wrong guesses and the hangman image
-    setWrong(st => st + (answer.includes(ltr) ? 0 : 1));
+    if (!answer.includes(ltr)) {
+      setWrong(st => {
+        const newWrong = st + 1;
+        if(newWrong === 7) setImage(images[6]);
+        else setImage(images[newWrong]); // Update image based on wrong guesses
+        return newWrong;
+      });
+    }
+  }
+   // Check if game is over (lost)
+   function gameOver() {
+    return wrong >= maxWrong;
+  }
 
-    // Update the image based on the number of wrong guesses
-    setImage(images[wrong + (answer.includes(ltr) ? 0 : 1)]);
+  // Check if game is won
+  function gameWon() {
+    return answer.split("").every(ltr => guess.has(ltr));
   }
   return (
     <>
-    <div className="main">
+    <div className="Hangman">
       <h1>Hangman</h1>
       <img src={image} alt="Hangman"></img>
       <div className="correct-guess">{guessed()}</div>
-      <div className="btns">{generateLetters()}</div>
+      {gameOver() ? (
+          <p className="msg-display">You Lose! The word was: {answer}</p>
+        ) : gameWon() ? (
+          <p className="msg-display">Congratulations! You won!</p>
+        ) : (
+          <div className="Hangman-btns">{generateLetters()}</div>
+        )}
+        <p className="msg-display">Wrong guesses: {wrong}</p>
     </div>
     </>
   )
